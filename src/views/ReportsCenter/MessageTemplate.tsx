f38e731172a6e84966dc6c00657478947d5699be
GreenAsJade <github@gregories.net>
2023-07-19 07:17:13 +0930

    Update MessageTemplate.tsx

--- src/views/ReportsCenter/MessageTemplate.tsx
+++ src/views/ReportsCenter/MessageTemplate.tsx
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* another test */
+
 import * as React from "react";
 import { PlayerCacheEntry } from "player_cache";
 import { Player } from "Player";
